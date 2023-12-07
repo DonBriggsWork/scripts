af0f3fdecd9dd60b105fb23f9325d3d4ba15286a
Don Briggs <DonBriggsWork@gmail.com>
2023-12-07 00:32:46 -0700

    Added some installation automation and remote file mount

--- example.sh
+++ example.sh
@@ -4,10 +4,27 @@
 # For: <mailto:osiris2050x@gmail.com>
 
 # Check if nmap is installed, and install if needed
+# inet 10.62.26.147  netmask 255.255.0.0  broadcast 10.62.255.255
 
+
+clear
+ printf "\n"
+printf "\n                                   IP Port Scanner\n"
+printf "============================================================================================\n"
+
+
+#- Check/Install nmap
 if ! which nmap > /dev/null; then
-    printf "installing nmap\n"
+    printf " * installing nmap...    "
     sudo apt install -y nmap > /dev/null
+    printf "[Done]\n"
 else
-    printf "nmap is already installed\n"
+    printf " * nmap already installed...    \n"
+
 fi
+
+printf "____________________________________________________________________________________________\n"
+printf "  Scanning for open ports. This will take a long time.\n"
+printf "  Output will be sent to file: scan_output.txt\n\n"
+nmap 10.62.26.1/16 > scan_output.txt
+printf "[Done]\n\n"
